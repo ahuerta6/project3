5f8d3dc00f68feb44aa28cf314c520b33a5197a2
ahuerta6 <ahuerta2@hawk.iit.edu>
2022-04-12 20:00:53 -0500

    fixing error?

--- js/pgscript.js
+++ js/pgscript.js
@@ -3,6 +3,6 @@ function initpage() {
  var t = document.getElementById('ti');
  pg.addEventListener('click', function(){
    alert("You're on the " + t.value + " page.");
- })
+ });
 }
 window.addEventListener('load', initpage);
