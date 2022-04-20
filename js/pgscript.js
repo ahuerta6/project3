ec4c7f9057b18df70df7418b2bcfd3b91f65c124
ahuerta6 <ahuerta2@hawk.iit.edu>
2022-04-20 00:16:14 -0500

    making value

--- js/pgscript.js
+++ js/pgscript.js
@@ -2,6 +2,7 @@ function initpage() {
  var pg = document.getElementById('page');
  var t = document.getElementById('ti');
  pg.addEventListener('click', function(){
+   t.innerHTML = t.value
    alert("You're on the " + t.value + " page.");
  });
 }
