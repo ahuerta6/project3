ddb0e40d0484b897e31a9252ef5e5c0cf3846e86
ahuerta6 <ahuerta2@hawk.iit.edu>
2022-04-25 00:32:21 -0500

    moving marker

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function initMap() {
 */
 
 addMarker(chicago);
-addMarker({lat:41.739685, lng:-87.554420});
+addMarker({lat:41.8781, lng:-87.58});
 
  function addMarker(coords) {
   const marker = new google.maps.Marker({
