89721eb046422cc40cb472efd335e530cf647058
ahuerta6 <ahuerta2@hawk.iit.edu>
2022-04-24 23:54:06 -0500

    making new marker

--- js/script.js
+++ js/script.js
@@ -4,25 +4,21 @@ $(document).ready(function(){
 
 // Initialize and add the map
 function initMap() {
- // The location 
+ // The location
  const chicago = { lat: 41.8781, lng: -87.6298 };
- // The map, centered 
- const map = new google.maps.Map(document.getElementById("map"), {
+ // The map, centered
+ const map = new google.maps.Map(document.getElementById('map'), {
    zoom: 11,
    center: chicago,
  });
- // The marker, positioned 
+ // The marker, positioned
  const marker = new google.maps.Marker({
    position: chicago,
    map: map,
  });
- //new marker
- const image =
-    "https://render.fineartamerica.com/images/rendered/default/print/8/6.5/break/images-medium-5/chicago-flag-mike-maher.jpg";
-  const beachMarker = new google.maps.Marker({
-    position: { lat: -41.83, lng: -87.62 },
-    map,
-    icon: image,
-  });
+
+ const marker1 = new google.maps.Marker({
+   position: { lat: 41.8782, lang: -87.6292}
+ })
 }
 window.addEventListener('load', initMap)
