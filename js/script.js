9535ece9d3c08feac84faa28e1229556062b56a5
ahuerta6 <ahuerta2@hawk.iit.edu>
2022-04-25 00:17:11 -0500

    adjusting map

--- js/script.js
+++ js/script.js
@@ -5,20 +5,21 @@ $(document).ready(function(){
 // Initialize and add the map
 function initMap() {
  // The location
- const chicago = { lat: 41.8781, lng: -87.6298 };
+ var options = {
  // The map, centered
- const map = new google.maps.Map(document.getElementById('map'), {
    zoom: 11,
-   center: chicago,
- });
- // The marker, positioned
- const marker = new google.maps.Marker({
-   position: chicago,
+   center: {lat: 41.8781, lng: -87.6298},
+ };
+
+//New map
+var map = new
+ google.maps.Map(document.getElementById('map'), options);
+
+ // adding maker
+ var marker = new google.maps.Marker({
+  position:{lat:41.8782, lang:-87.6292}
    map: map,
  });
-
- const marker1 = new google.maps.Marker({
-   position: { lat: 41.8782, lang: -87.6292}
- })
 }
+
 window.addEventListener('load', initMap)
