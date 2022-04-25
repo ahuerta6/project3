531c6e7fe322e12e57cd1b22c73c00ac97423326
ahuerta6 <ahuerta2@hawk.iit.edu>
2022-04-25 01:07:25 -0500

    adding content to markers

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
 // Initialize and add the map
 function initMap() {
- // The location
+  // The location
  const chicago = { lat: 41.8781, lng: -87.6298 };
  // The map, centered
  const map = new google.maps.Map(document.getElementById('map'), {
@@ -20,11 +20,13 @@ function initMap() {
 
 addMarker({
   coords:chicago,
-  iconImage: 'https://image.freepik.com/free-photo/flag-chicago-city-illinois-us_2227-1121.jpg'
+  iconImage: 'https://image.freepik.com/free-photo/flag-chicago-city-illinois-us_2227-1121.jpg',
+  content: '<h1>This is the middle of Chicago</h1>',
 });
 addMarker({
   coords:{lat:41.83, lng:-87.6298},
-  iconImage: 'https://designlooter.com/images/hawk-svg-7.png'
+  iconImage: 'https://designlooter.com/images/hawk-svg-7.png',
+  content: '<p>IIT is located in the south of Chicago</p>',
 });
 
  function addMarker(props) {
@@ -32,9 +34,21 @@ addMarker({
     position: props.coords,
     map: map,
   });
+
   //check for icon
   if(props.iconImage){
     marker.setIcon(props.iconImage);
+  }
+
+ //check for content
+ if(props.content){
+   var infoWindow = new google.maps.InfoWindow({
+   content:props.content
+ });
+
+ marker.addListener('click', function(){
+   infoWindow.open(map, marker);
+ });
   }
  }
 }
