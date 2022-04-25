87b5ca6294edbc846d9f2b1ccfb53f144afe69af
ahuerta6 <ahuerta2@hawk.iit.edu>
2022-04-24 22:29:38 -0500

    making new marker

--- js/script.js
+++ js/script.js
@@ -4,20 +4,25 @@ $(document).ready(function(){
 
 // Initialize and add the map
 function initMap() {
- // The location of Uluru
+ // The location 
  const chicago = { lat: 41.8781, lng: -87.6298 };
- // The map, centered at Uluru
+ // The map, centered 
  const map = new google.maps.Map(document.getElementById("map"), {
    zoom: 11,
    center: chicago,
  });
- // The marker, positioned at Uluru
+ // The marker, positioned 
+ const marker = new google.maps.Marker({
+   position: chicago,
+   map: map,
+ });
+ //new marker
  const image =
     "https://render.fineartamerica.com/images/rendered/default/print/8/6.5/break/images-medium-5/chicago-flag-mike-maher.jpg";
   const beachMarker = new google.maps.Marker({
-   position: chicago,
-   map: map,
-   icon: 'image'
- });
+    position: { lat: -41.83, lng: -87.62 },
+    map,
+    icon: image,
+  });
 }
 window.addEventListener('load', initMap)
