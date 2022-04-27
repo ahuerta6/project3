33fef27d2c52dcf85e5ab7ae1cc270f97af46707
ahuerta6 <ahuerta2@hawk.iit.edu>
2022-04-26 23:54:54 -0500

    maybe this will work

--- js/pgscript.js
+++ js/pgscript.js
@@ -2,7 +2,7 @@ function initpage() {
  var pg = document.getElementById('page');
  var t = document.getElementById('ti');
  pg.addEventListener('click', function(){
-   alert("You're on the " + t.value + " page.");
+   alert("You're on the " + t + " page.");
  });
 }
 window.addEventListener('load', initpage);
