5a8253298b3f6546835408e24c7a849282a2b50f
ahuerta6 <ahuerta2@hawk.iit.edu>
2022-04-27 00:10:30 -0500

    deleting java page

--- js/pgscript.js
+++
@@ -1,8 +0,0 @@
-function initpage() {
- var pg = document.getElementById('page');
- var t = document.getElementById('ti');
- pg.addEventListener('click', function(){
-   alert("URL: " + t);
- });
-}
-window.addEventListener('load', initpage);
--- js/script.js
+++ js/script.js
@@ -60,4 +60,12 @@ addMarker({
   });
  }
 }
+
+function initpage() {
+  var pg = document.getElementById('page');
+  var t = document.getElementById('ti');
+  pg.addEventListener('click', function(){
+    alert("URL: " + t);
+  });
+ }
 window.addEventListener('load', initMap)
